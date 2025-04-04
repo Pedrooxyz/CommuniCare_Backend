a89c2a19860633a0669a4c8ed4f94d9ceb40354d
RuiMirandaESI <73992907+RuiMirandaESI@users.noreply.github.com>
2025-04-04 16:37:02 +0100

    jwt token

--- CommuniCare/CommuniCare/CommuniCare/Program.cs
+++ CommuniCare/CommuniCare/CommuniCare/Program.cs
@@ -1,5 +1,8 @@
 using CommuniCare.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,7 +16,27 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<CommuniCareContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ConStr")));
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        // Here, configure how the token should be validated
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,                
+            ValidateAudience = true,             
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],       
+            ValidAudience = builder.Configuration["Jwt:Audience"],   
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])) // secret key from config
+        };
+    });
+
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -22,6 +45,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();

52762114783b78e67633b94e86d879b615bca4c0
Pedro Ribeiro <151522723+Pedrooxyz@users.noreply.github.com>
2025-04-29 16:51:26 +0100

    Edição nos controllers dos voluntariados e dos pedidos de ajuda

--- CommuniCare/CommuniCare/CommuniCare/obj/Debug/net8.0/CommuniCare.AssemblyInfo.cs
+++ CommuniCare/CommuniCare/CommuniCare/obj/Debug/net8.0/CommuniCare.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommuniCare")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9431d2cdeaaf86f466b04e187d6dc206f595af9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0071ad07162e965882c07791a32058f53e2dc64")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommuniCare")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommuniCare")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

a5a4373652a882bb467fb2ec11553cc8808f1e3c
RuiMirandaESI <73992907+RuiMirandaESI@users.noreply.github.com>
2025-05-02 10:25:48 +0100

    Teste Voluntariados

--- CommuniCare/CommuniCare/CommuniCareTests/obj/Debug/net8.0/CommuniCareTests.AssemblyInfo.cs
+++ CommuniCare/CommuniCare/CommuniCareTests/obj/Debug/net8.0/CommuniCareTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommuniCareTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+beb742497135ca4552d92c8baed6fb3154d9bf93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adc4a6478f5891e42c0dc2392866f0f43ad8f307")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommuniCareTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommuniCareTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

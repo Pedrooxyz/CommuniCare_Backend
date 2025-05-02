b5132606d87b29c03277d5e1327397c705d46697
JoaoBarbosaaa <jpjbcmjg@gmail.com>
2025-05-02 16:47:02 +0100

    Conclusão da documentação dos testes

--- CommuniCare/CommuniCare/CommuniCareTests/ArtigoTests.cs
+++ CommuniCare/CommuniCare/CommuniCareTests/ArtigoTests.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿/// <summary>
+/// Namespace que contém os testes unitários da aplicação CommuniCare.
+/// Utiliza o framework MSTest e Moq para simular interações com a base de dados e testar o comportamento dos métodos do controlador LojasController.
+/// </summary>
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -21,6 +26,10 @@ using SendGrid.Helpers.Mail;
 
 namespace CommuniCareTests
 {
+    /// <summary>
+    /// Classe de testes para os métodos do controlador <see cref="ArtigosController"/>.
+    /// Contém testes unitários que verificam o comportamento esperado dos métodos relacionados a artigos.
+    /// </summary>
     [TestClass]
     public class ArtigosTests
     {
@@ -31,6 +40,10 @@ namespace CommuniCareTests
         private Mock<DbSet<Utilizador>> _mockUtilizadoresDbSet;
         private Mock<CommuniCareContext> _mockContext;
 
+        /// <summary>
+        /// Inicializa o ambiente de teste antes da execução de cada método de teste.
+        /// Cria mocks para os DbSet e para o contexto do banco de dados, e instancia o controller a ser testado.
+        /// </summary>
         [TestInitialize]
         public void Setup()
         {
@@ -51,10 +64,15 @@ namespace CommuniCareTests
 
         #region GetArtigosDisponiveis
 
+        /// <summary>
+        /// Testa se o método GetArtigosDisponiveis retorna um resultado Ok com a lista de artigos disponíveis quando existem artigos disponíveis na base de dados.
+        /// </summary>
+        /// <returns>Uma tarefa que representa a execução do teste unitário.</returns>
+
         [TestMethod]
         public async Task GetArtigosDisponiveis_ReturnsOk_WhenArtigosExist()
         {
-            // Arrange
+
             var artigosList = new List<Artigo>
             {
                 new Artigo { ArtigoId = 1, NomeArtigo = "Artigo 1", Estado = EstadoArtigo.Disponivel },
@@ -64,10 +82,10 @@ namespace CommuniCareTests
             var mockArtigosDbSet = artigosList.AsQueryable().BuildMockDbSet();
             _mockContext.Setup(c => c.Artigos).Returns(mockArtigosDbSet.Object);
 
-            // Act
+
             var result = await _controller.GetArtigosDisponiveis();
 
-            // Assert
+
             Assert.IsInstanceOfType(result.Result, typeof(OkObjectResult));
             var okResult = result.Result as OkObjectResult;
             Assert.AreEqual(2, ((List<Artigo>)okResult.Value).Count);
@@ -77,10 +95,15 @@ namespace CommuniCareTests
 
         #region PublicarArtigos
 
+        /// <summary>
+        /// Testa se o método PublicarArtigo retorna CreatedAtActionResult quando os dados fornecidos são válidos e o artigo é publicado com sucesso.
+        /// </summary>
+        /// <returns>Uma tarefa que representa a execução do teste unitário.</returns>
+
         [TestMethod]
         public async Task PublicarArtigo_ValidData_ReturnsOk()
         {
-            /*  Arrange  */
+
             var dto = new ArtigoDto
             {
                 NomeArtigo = "Artigo Teste",
@@ -90,29 +113,29 @@ namespace CommuniCareTests
                 FotografiaArt = "foto.jpg"
             };
 
-            
+
             var user = new Utilizador { UtilizadorId = 2, NomeUtilizador = "Admin" };
 
             var utilizadoresDb = new[] { user }
                                  .AsQueryable()
                                  .BuildMockDbSet();
 
-            
+
             utilizadoresDb
                 .Setup(d => d.FindAsync(It.IsAny<object[]>()))
                 .ReturnsAsync(user);
 
-            
+
             var lojaAtiva = new Loja { LojaId = 10, Estado = EstadoLoja.Ativo };
 
             var lojasDb = new[] { lojaAtiva }
                           .AsQueryable()
                           .BuildMockDbSet();
 
-            
+
             var artigosDb = new Mock<DbSet<Artigo>>();
 
-            
+
             var ctx = new Mock<CommuniCareContext>();
             ctx.Setup(c => c.Utilizadores).Returns(utilizadoresDb.Object);
             ctx.Setup(c => c.Lojas).Returns(lojasDb.Object);
@@ -120,7 +143,7 @@ namespace CommuniCareTests
             ctx.Setup(c => c.SaveChangesAsync(It.IsAny<CancellationToken>()))
                .ReturnsAsync(1);
 
-            
+
             var controller = new ArtigosController(ctx.Object)
             {
                 ControllerContext = new ControllerContext
@@ -135,10 +158,10 @@ namespace CommuniCareTests
                 }
             };
 
-            /*  Act  */
+
             var result = await controller.PublicarArtigo(dto);
 
-            /*  Assert  */
+
             Assert.IsInstanceOfType(result.Result, typeof(CreatedAtActionResult));
 
             var created = (CreatedAtActionResult)result.Result;
@@ -149,7 +172,7 @@ namespace CommuniCareTests
             Assert.AreEqual(dto.CustoCares, resposta.CustoCares);
             Assert.AreEqual(dto.QuantidadeDisponivel, resposta.QuantidadeDisponivel);
 
-            
+
             artigosDb.Verify(d => d.Add(It.IsAny<Artigo>()), Times.Once);
             ctx.Verify(c => c.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
         }
@@ -158,46 +181,59 @@ namespace CommuniCareTests
 
         #region Indisponibilizar
 
+        /// <summary>
+        /// Testa se o método IndisponibilizarArtigo retorna OkObjectResult com a mensagem apropriada quando um administrador marca um artigo como indisponível.
+        /// </summary>
+        /// <returns>Uma tarefa que representa a execução do teste unitário.</returns>
+
         [TestMethod]
-public async Task IndisponibilizarArtigo_Admin_ReturnsOk()
-{
-    // Arrange
-    var artigo = new Artigo { ArtigoId = 1, NomeArtigo = "Artigo Teste", Estado = EstadoArtigo.Disponivel };
-    var utilizador = new Utilizador { UtilizadorId = 2, TipoUtilizadorId = 2, NomeUtilizador = "Admin" };
+        public async Task IndisponibilizarArtigo_Admin_ReturnsOk()
+        {
 
-    _mockContext.Setup(c => c.Artigos.FindAsync(1)).ReturnsAsync(artigo);
-    _mockContext.Setup(c => c.Utilizadores.FindAsync(2)).ReturnsAsync(utilizador);
-    _mockContext.Setup(c => c.SaveChangesAsync(default)).ReturnsAsync(1);
+            var artigo = new Artigo { ArtigoId = 1, NomeArtigo = "Artigo Teste", Estado = EstadoArtigo.Disponivel };
+            var utilizador = new Utilizador { UtilizadorId = 2, TipoUtilizadorId = 2, NomeUtilizador = "Admin" };
 
-    // Simula utilizador autenticado
-    var claims = new List<Claim> { new Claim(ClaimTypes.NameIdentifier, "2") };
-    var identity = new ClaimsIdentity(claims, "TestAuth");
-    var principal = new ClaimsPrincipal(identity);
-    _controller.ControllerContext = new ControllerContext { HttpContext = new DefaultHttpContext { User = principal } };
+            _mockContext.Setup(c => c.Artigos.FindAsync(1)).ReturnsAsync(artigo);
+            _mockContext.Setup(c => c.Utilizadores.FindAsync(2)).ReturnsAsync(utilizador);
+            _mockContext.Setup(c => c.SaveChangesAsync(default)).ReturnsAsync(1);
 
-    // Act
-    var result = await _controller.IndisponibilizarArtigo(1);
 
-    // Assert
-    Assert.IsInstanceOfType(result, typeof(OkObjectResult));
-    var okResult = result as OkObjectResult;
-    Assert.AreEqual("Artigo indisponibilizado com sucesso.", okResult.Value);
-}
+            var claims = new List<Claim> { new Claim(ClaimTypes.NameIdentifier, "2") };
+            var identity = new ClaimsIdentity(claims, "TestAuth");
+            var principal = new ClaimsPrincipal(identity);
+            _controller.ControllerContext = new ControllerContext { HttpContext = new DefaultHttpContext { User = principal } };
+
+
+            var result = await _controller.IndisponibilizarArtigo(1);
+
+
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = result as OkObjectResult;
+            Assert.AreEqual("Artigo indisponibilizado com sucesso.", okResult.Value);
+        }
 
         #endregion
 
         #region ReporStock
 
+        /// <summary>
+        /// Classe de contexto de banco de dados usada para testes unitários com uma base de dados em memória.
+        /// </summary>
         private sealed class TestContext : CommuniCareContext
         {
             public TestContext(DbContextOptions<CommuniCareContext> opts) : base(opts) { }
             protected override void OnConfiguring(DbContextOptionsBuilder _) { /* no-op */ }
         }
 
+        /// <summary>
+        /// Testa se o método ReporStock retorna OkObjectResult e atualiza corretamente a quantidade disponível do artigo
+        /// quando os dados são válidos e o utilizador está autenticado.
+        /// </summary>
+        /// <returns>Uma tarefa que representa a execução do teste unitário.</returns>
         [TestMethod]
         public async Task ReporStock_ReturnsOk_WhenSuccessful()
         {
-            /*  Arrange  */
+
             const int userId = 1;
             const int lojaId = 5;
             const int artigoId = 10;
@@ -208,7 +244,7 @@ public async Task IndisponibilizarArtigo_Admin_ReturnsOk()
 
             await using var ctx = new TestContext(opts);
 
-            
+
             ctx.Utilizadores.Add(new Utilizador { UtilizadorId = userId });
             ctx.Lojas.Add(new Loja { LojaId = lojaId, Estado = EstadoLoja.Ativo });
             ctx.Artigos.Add(new Artigo
@@ -239,17 +275,17 @@ public async Task IndisponibilizarArtigo_Admin_ReturnsOk()
 
             var dto = new ReporStockDto { Quantidade = 5 };
 
-            /*  Act  */
+
             var result = await controller.ReporStock(artigoId, dto);
 
-            /*  Assert  */
+
             Assert.IsInstanceOfType(result.Result, typeof(OkObjectResult));
             var ok = (OkObjectResult)result.Result;
             var resposta = ok.Value as ArtigoRespostaDto;
 
             Assert.IsNotNull(resposta);
             Assert.AreEqual(artigoId, resposta.ArtigoId);
-            Assert.AreEqual(7, resposta.QuantidadeDisponivel);   // 2 + 5
+            Assert.AreEqual(7, resposta.QuantidadeDisponivel);
 
             var recarregado = await ctx.Artigos.FindAsync(artigoId);
             Assert.AreEqual(7, recarregado.QuantidadeDisponivel);
@@ -264,19 +300,23 @@ public async Task IndisponibilizarArtigo_Admin_ReturnsOk()
 
         #region GetArtigosDisponiveis
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         [TestMethod]
         public async Task GetArtigosDisponiveis_ReturnsOk_WhenNoArtigosExist()
         {
-            // Arrange
-            var artigosList = new List<Artigo>(); // Lista vazia
+
+            var artigosList = new List<Artigo>();
 
             var mockArtigosDbSet = artigosList.AsQueryable().BuildMockDbSet();
             _mockContext.Setup(c => c.Artigos).Returns(mockArtigosDbSet.Object);
 
-            // Act
+
             var result = await _controller.GetArtigosDisponiveis();
 
-            // Assert
+
             Assert.IsInstanceOfType(result.Result, typeof(OkObjectResult));
             var okResult = result.Result as OkObjectResult;
             Assert.IsNotNull(okResult);
@@ -287,13 +327,17 @@ public async Task IndisponibilizarArtigo_Admin_ReturnsOk()
 
         #region PublicarArtigos
 
-[TestMethod]
-public async Task PublicarArtigo_ReturnsBadRequest_WhenNoActiveLoja()
+        /// <summary>
+        /// Testa se o método PublicarArtigo retorna BadRequestObjectResult quando não existe nenhuma loja ativa no sistema.
+        /// </summary>
+        /// <returns>Uma tarefa que representa a execução do teste unitário.</returns>
+        [TestMethod]
+        public async Task PublicarArtigo_ReturnsBadRequest_WhenNoActiveLoja()
         {
-            /*  Arrange  */
+
             const int utilizadorId = 1;
 
-            
+
             _controller.ControllerContext = new ControllerContext
             {
                 HttpContext = new DefaultHttpContext
@@ -305,7 +349,7 @@ public async Task PublicarArtigo_ReturnsBadRequest_WhenNoActiveLoja()
                 }
             };
 
-           
+
             var utilizador = new Utilizador { UtilizadorId = utilizadorId };
             var utilizadoresDb = new[] { utilizador }
                                  .AsQueryable()
@@ -317,13 +361,13 @@ public async Task PublicarArtigo_ReturnsBadRequest_WhenNoActiveLoja()
 
             _mockContext.Setup(c => c.Utilizadores).Returns(utilizadoresDb.Object);
 
-            
-            var lojasDb = new List<Loja>()            
+
+            var lojasDb = new List<Loja>()
                           .AsQueryable()
                           .BuildMockDbSet();
             _mockContext.Setup(c => c.Lojas).Returns(lojasDb.Object);
 
-            
+
             var dto = new ArtigoDto
             {
                 NomeArtigo = "Novo Artigo",
@@ -333,10 +377,10 @@ public async Task PublicarArtigo_ReturnsBadRequest_WhenNoActiveLoja()
                 FotografiaArt = "foto.jpg"
             };
 
-            /*  Act  */
+
             var result = await _controller.PublicarArtigo(dto);
 
-            /*  Assert  */
+
             Assert.IsInstanceOfType(result.Result, typeof(BadRequestObjectResult));
             var bad = (BadRequestObjectResult)result.Result;
             Assert.AreEqual("Não existe nenhuma loja ativa no momento.", bad.Value);
@@ -346,10 +390,14 @@ public async Task PublicarArtigo_ReturnsBadRequest_WhenNoActiveLoja()
 
         #region Indisponibilizar
 
+        /// <summary>
+        /// Testa se o método IndisponibilizarArtigo retorna ForbidResult quando o utilizador autenticado não é um administrador.
+        /// </summary>
+        /// <returns>Uma tarefa que representa a execução do teste unitário.</returns>
         [TestMethod]
         public async Task IndisponibilizarArtigo_ReturnsForbid_WhenUserIsNotAdmin()
         {
-            // Arrange
+
             int artigoId = 1;
             var userIdClaim = new Claim(ClaimTypes.NameIdentifier, "1");
             var claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new[] { userIdClaim }));
@@ -358,25 +406,30 @@ public async Task PublicarArtigo_ReturnsBadRequest_WhenNoActiveLoja()
                 HttpContext = new DefaultHttpContext { User = claimsPrincipal }
             };
 
-            // Act
+
             var result = await _controller.IndisponibilizarArtigo(artigoId);
 
-            // Assert
+
             Assert.IsInstanceOfType(result, typeof(ForbidResult));
         }
 
         #endregion
-        
+
         #region ReporStock
+
+        /// <summary>
+        /// Testa se o método ReporStock retorna BadRequest quando a quantidade informada é inválida (menor ou igual a zero).
+        /// </summary>
+        /// <returns>Uma tarefa que representa a execução do teste unitário.</returns>
 
         [TestMethod]
         public async Task ReporStock_ReturnsBadRequest_WhenQuantidadeInvalid()
         {
-            /*  Arrange  */
+
             const int utilizadorId = 1;
             const int artigoId = 10;
 
-            
+
             _controller.ControllerContext = new ControllerContext
             {
                 HttpContext = new DefaultHttpContext
@@ -388,7 +441,7 @@ public async Task PublicarArtigo_ReturnsBadRequest_WhenNoActiveLoja()
                 }
             };
 
-           
+
             var utilizador = new Utilizador { UtilizadorId = utilizadorId };
 
             var utilizadoresDb = new[] { utilizador }
@@ -401,7 +454,7 @@ public async Task PublicarArtigo_ReturnsBadRequest_WhenNoActiveLoja()
 
             _mockContext.Setup(c => c.Utilizadores).Returns(utilizadoresDb.Object);
 
-           
+
             var lojaAtiva = new Loja { LojaId = 5, Estado = EstadoLoja.Ativo };
 
             var lojasDb = new[] { lojaAtiva }
@@ -410,13 +463,13 @@ public async Task PublicarArtigo_ReturnsBadRequest_WhenNoActiveLoja()
 
             _mockContext.Setup(c => c.Lojas).Returns(lojasDb.Object);
 
-            
+
             var dto = new ReporStockDto { Quantidade = 0 };
 
-            /*  Act  */
+
             var result = await _controller.ReporStock(artigoId, dto);
 
-            /*  Assert  */
+
             Assert.IsInstanceOfType(result.Result, typeof(BadRequestObjectResult));
             var bad = (BadRequestObjectResult)result.Result;
             Assert.AreEqual("A quantidade deve ser maior que zero.", bad.Value);
